2c6e2befd7e5fa595a2b8dbabb6193fda54f4361
GLCVLL <g.ioele96@outlook.it>
2023-07-24 17:30:42 +0200

    instantiated categories

--- categories.php
+++ categories.php
@@ -6,3 +6,16 @@ class Categoria {
         $this->nome = $nome;
     }
 }
+
+// instanziare le categorie e restituirle in array
+function getCategories() {
+    $categorie = array(
+        new Categoria("Cane"),
+        new Categoria("Gatto"),
+        new Categoria("Uccelli"),
+        new Categoria("Pesci"),
+    );
+
+    return $categorie;
+}
+?>
